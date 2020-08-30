8a98b27d6badf85ef11e89bbdb2451c14514b847
Peter Locke <peter@kiind.me>
2020-08-29 23:40:30 -0700

    main state  logic flow complete

--- src/constants.h
+++ src/constants.h
@@ -12,9 +12,10 @@ const String codeversion = "1.0";
 //states
 const int STATE_BOOTED_UP = 0;
 const int STATE_WAITING_TO_START = 10;
+const int STATE_COUNTDOWN = 15;
 const int STATE_GOING = 20;
+const int STATE_WIN = 25;
 const int STATE_FAILED = 30;
-const int STATE_SUCCESS = 40;
 
 
 //debug flag
@@ -27,14 +28,11 @@ const int PIN_STARTCRADLE = A1;
 const int PIN_GAMEWIRE = A2;
 
 //output pins
-const int PIN_GOING_LED_GREEN = 3; //PWM Pin
-const int PIN_YELLOW_LED = 4;
-const int PIN_FAILED_LED= 2;
-const int PIN_SPEAKER = 5;
+const int PIN_GOING_LED_GREEN = 9; //PWM Pin
+const int PIN_YELLOW_LED = 10;
+const int PIN_FAILED_LED= 8;
+const int PIN_SPEAKER = 13;
 
-//global variables
-extern int state;
-extern long timeSinceStart;
 
 //sound constants
 const int NOTE_FREQUENCY_FAIL = 80;
@@ -44,6 +42,9 @@ const int NOTE_FREQUENCY_SUCCESS_ARRAY[] = {200,300,400};
 //timing constants
 const long ERROR_TONE_LENGTH_MILLIS = 1500;
 const int SUCCESS_TONE_LENGTH_MS = 350;
+const long COUNTDOWN_STEPLENGTH_MS = 500;
+const int COUNTDOWN_NUMSTEPS = 3;
+const long DEBOUNCE_THRESHOLD_MS = 100;
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include <Arduino.h>
 #include <constants.h>
+#include <lcdhelper.h>
+
+
+//global variables
+int currentState = STATE_BOOTED_UP;
+
+long timeSinceStartTimer;
+long countDownTimer;
+long gameOverDisplayTimer;
+int currentCountDownStep;
+long debounceTimer;
+int lastDebouncePinState = -1;
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void resetVariables() {
+
+ timeSinceStartTimer = 0L;
+ countDownTimer = 0L;
+ gameOverDisplayTimer = 0L;
+ currentCountDownStep = COUNTDOWN_NUMSTEPS;
+}
 
 void testLEDPin(int ledPIN)
 {
@@ -8,9 +31,69 @@ void testLEDPin(int ledPIN)
   digitalWrite(ledPIN, LOW);
 }
 
+long getElapsedMillisForTimerVariable(long timerVariable) {
+  return millis() - timerVariable;
+}
+
+int getElapsedSecondsForTImerVariable(long timerVariable) {
+  return getElapsedMillisForTimerVariable(timerVariable)/1000;
+}
+
+
+
+void transitionToState(int newState) {
+  if (currentState == newState) {
+    return;
+  } 
+
+  switch (newState)
+  {
+  case STATE_WAITING_TO_START:
+    resetVariables();
+    setBothLCDLines("Touch Wand to", "*Start* to begin", lcd);
+    break;
+
+   case STATE_COUNTDOWN:
+    lcd.clear();
+    setTopLine("Get Ready!", lcd);
+    currentCountDownStep = COUNTDOWN_NUMSTEPS;
+    setBottomLine("In "+String(currentCountDownStep), lcd);
+    countDownTimer = millis();
+    break;
+
+  case STATE_GOING:
+    timeSinceStartTimer = millis();
+    lcd.clear();
+    setTopLine("Go (Carefully) ;)", lcd);
+    break;
+
+  case STATE_WIN:
+    /* code */
+    break;
+
+  case STATE_FAILED:
+    gameOverDisplayTimer = millis();
+    break;
+
+
+  
+  default:
+    break;
+  }
+
+  Serial.println("Transitioning state "+String(currentState)+" to "+String(newState));
+  currentState = newState;
+
+
+
+  
+}
+
 void setup() {
   // put your setup code here, to run once:
-  //LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+Serial.begin(9600);
+lcd.begin(16,2);
+
 
   for (int i = 2; i<= 13; i++) {
     pinMode(i, OUTPUT);
@@ -22,12 +105,79 @@ void setup() {
   tone(PIN_SPEAKER, 523);
   delay(250);
   noTone(PIN_SPEAKER);
+
+  transitionToState(STATE_WAITING_TO_START);
 }
-
-
 
 void loop() {
   // put your main code here, to run repeatedly:
+  switch (currentState)
+  {
+  case STATE_WAITING_TO_START:
+    if (digitalRead(PIN_STARTCRADLE) == HIGH) {
+      transitionToState(STATE_COUNTDOWN);
+    }
+    break;
+
+  case STATE_GOING:
+    if (digitalRead(PIN_GAMEWIRE) == HIGH) {
+      setBothLCDLines("** WIRE TOUCHED ** ","you lasted "+String(getElapsedSecondsForTImerVariable(timeSinceStartTimer))+"s", lcd);
+      transitionToState(STATE_FAILED);
+    }
+
+    if (getElapsedMillisForTimerVariable(timeSinceStartTimer) % 100 == 0)
+    {
+      setBottomLine(String(getElapsedSecondsForTImerVariable(timeSinceStartTimer))+"."+String((getElapsedMillisForTimerVariable(timeSinceStartTimer)-getElapsedSecondsForTImerVariable(timeSinceStartTimer)*1000L)/100L), lcd);
+    }
+    break;
+
+  case STATE_WIN:
+    /* code */
+    break;
+
+  case STATE_FAILED:
+      if (digitalRead(PIN_STARTCRADLE) == HIGH) {
+        transitionToState(STATE_COUNTDOWN);
+      }
+        if(getElapsedSecondsForTImerVariable(gameOverDisplayTimer) > 5) {
+      transitionToState(STATE_WAITING_TO_START);
+    }
+    break;
+
+  case STATE_COUNTDOWN:
+    
+    int currentStartPinState = digitalRead(PIN_STARTCRADLE);
+    if (currentStartPinState != lastDebouncePinState) {
+      debounceTimer = millis();
+      lastDebouncePinState = currentStartPinState;
+
+      Serial.println("In Debounce. Pinstate is "+String(currentStartPinState)+" lastPinState is "+String(lastDebouncePinState));
+    }
+
+    if ((millis() - debounceTimer) > DEBOUNCE_THRESHOLD_MS)
+    {
+      Serial.println("In Debounce Else. Pinstate is "+String(currentStartPinState)+" lastPinState is "+String(lastDebouncePinState));
+
+      if( currentStartPinState == LOW) { //started too early
+        setBothLCDLines("** DISQUALIFIED ** ","Started too early", lcd);
+        transitionToState(STATE_FAILED);
+      }
+    }
+
+    Serial.println("millis minus debounce is "+(String)(millis() - debounceTimer));
+
+    if (getElapsedMillisForTimerVariable(countDownTimer) > COUNTDOWN_STEPLENGTH_MS) {
+      countDownTimer = millis();
+      currentCountDownStep --;
+      setBottomLine("In "+String(currentCountDownStep), lcd);
+    }
+    
+    if (currentCountDownStep == 0) {
+      transitionToState(STATE_GOING);
+    }
+
+    break;
+  }
 
   
 
